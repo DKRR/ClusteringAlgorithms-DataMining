7c5b0862d8418decb81d9c8aeeea195e0373b141
ramcode <ramesh.thetakali@gmail.com>
2016-10-29 00:17:35 -0400

    Kmeans Initial Commit

---
+++ src/com/ub/cse601/project2/clustering/KMeans.java
@@ -0,0 +1,124 @@
+package com.ub.cse601.project2.clustering;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
+import java.util.stream.Stream;
+
+/**
+ * Created by VenkataRamesh on 10/28/2016.
+ */
+public class KMeans {
+    private Integer maxIterations;
+    private Integer initialCentroids;
+    private String fileName;
+    private double[][] dataMatrix;
+    private Map<Integer, List<double[][]>> clusters;
+    private int objectCount;
+    private int attributeCount;
+
+
+    public KMeans(Integer initialCentroids, String fileName, Integer maxIterations) {
+        this.initialCentroids = initialCentroids;
+        this.fileName = fileName;
+        this.maxIterations = maxIterations;
+    }
+
+    public void readGeneDataSet(String path) throws IOException, FileNotFoundException {
+        Path filePath = null;
+        try {
+            filePath = Paths.get(path, fileName);
+            Stream<String> genes = Files.lines(filePath, StandardCharsets.UTF_8);
+            List<String> geneData = Files.readAllLines(filePath, StandardCharsets.UTF_8);
+            int rows = geneData.size();
+            this.objectCount = rows;
+            int columns = geneData.get(0).split("\t").length;
+            this.attributeCount = columns - 2;
+            dataMatrix = new double[rows][columns];
+            for (int i = 0; i < rows; i++) {
+                String[] geneAttributes = geneData.get(i).split("\t");
+                for (int j = 0; j < columns - 1; j++) {
+                    if (j == 0) dataMatrix[i][j] = Double.parseDouble(geneAttributes[j]);
+                    else dataMatrix[i][j] = Double.parseDouble(geneAttributes[j + 1]);
+                }
+                dataMatrix[i][columns - 1] = Double.parseDouble(geneAttributes[1]);
+            }
+        } catch (Exception ex) {
+
+        }
+    }
+
+    public double[][] calculateDistanceMatrix() {
+        double[][] distanceMatrix = new double[dataMatrix.length][dataMatrix.length];
+        for (int i = 0; i < dataMatrix.length - 1; i++) {
+            for (int j = i + 1; j < dataMatrix.length; j++) {
+                double[] obj1 = dataMatrix[i];
+                double[] obj2 = dataMatrix[j];
+                double squaredSum = 0;
+                for (int dim = 1; dim < obj1.length - 1; dim++) {
+                    squaredSum += Math.pow(obj1[dim] - obj2[dim], 2);
+                }
+                double eucDistance = Math.sqrt(squaredSum);
+                distanceMatrix[i][j] = eucDistance;
+                distanceMatrix[j][i] = eucDistance;
+            }
+        }
+        return distanceMatrix;
+    }
+
+    public double[][] initKMeans() {
+        double[][] kMeans = new double[initialCentroids][];
+        double[][] clusterIndex = new double[dataMatrix.length][initialCentroids];
+        int k = 0;
+        Random rand = new Random();
+        List<Integer> clusterIndices = new ArrayList<Integer>();
+
+        this.clusters = new HashMap<Integer, List<double[][]>>();
+        while (k < initialCentroids) {
+            int centroidIndex = rand.nextInt(dataMatrix.length);
+            if (clusterIndices.contains(centroidIndex)) continue;
+            else {
+                clusters.put(k + 1, new ArrayList<double[][]>());
+                clusterIndices.add(centroidIndex);
+                kMeans[k] = dataMatrix[centroidIndex];
+                k++;
+            }
+        }
+        System.out.println(kMeans);
+        return kMeans;
+    }
+
+    public void runKMeans(double[][] kMeans) {
+        boolean converged = false;
+        int iterations = 1;
+
+        while (!converged && iterations <= maxIterations) {
+            for (int i = 0; i < dataMatrix.length; i++) {
+                double squaredDistance = 0;
+                double eucdDis = 0;
+                double minDistance = Double.MAX_VALUE;
+                int minClusterIndex = 0;
+                for (int k = 0; k < kMeans.length; k++) {
+                    for (int j = 1; j <= attributeCount; j++) {
+                        squaredDistance += Math.pow(dataMatrix[i][j] - kMeans[k][j], 2);
+                    }
+                    eucdDis = Math.sqrt(squaredDistance);
+                    if (eucdDis < minDistance) {
+                        minDistance = eucdDis;
+                        minClusterIndex = k + 1;
+                    }
+                    squaredDistance = 0;
+                }
+                System.out.println("Data Point: " + dataMatrix[i][0] + " Min Distance->" + minDistance + " ClusterIndex->" + minClusterIndex);
+            }
+        }
+    }
+
+
+}
