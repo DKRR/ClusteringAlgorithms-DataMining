e9f1a3d44b5233963bf2775e662b1632d1cf0fef
ramcode <ramesh.thetakali@gmail.com>
2016-10-29 11:09:30 -0400

    Kmeans Working Code

--- src/com/ub/cse601/project2/clustering/KMeans.java
+++ src/com/ub/cse601/project2/clustering/KMeans.java
@@ -2,6 +2,8 @@ package com.ub.cse601.project2.clustering;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -22,6 +24,7 @@ public class KMeans {
     private Map<Integer, List<double[][]>> clusters;
     private int objectCount;
     private int attributeCount;
+    private int clusterIndex;
 
 
     public KMeans(Integer initialCentroids, String fileName, Integer maxIterations) {
@@ -39,8 +42,9 @@ public class KMeans {
             int rows = geneData.size();
             this.objectCount = rows;
             int columns = geneData.get(0).split("\t").length;
+            clusterIndex = columns;
             this.attributeCount = columns - 2;
-            dataMatrix = new double[rows][columns];
+            dataMatrix = new double[rows][columns + 1];
             for (int i = 0; i < rows; i++) {
                 String[] geneAttributes = geneData.get(i).split("\t");
                 for (int j = 0; j < columns - 1; j++) {
@@ -86,19 +90,21 @@ public class KMeans {
             else {
                 clusters.put(k + 1, new ArrayList<double[][]>());
                 clusterIndices.add(centroidIndex);
-                kMeans[k] = dataMatrix[centroidIndex];
+                kMeans[k] = Arrays.copyOfRange(dataMatrix[centroidIndex], 1, attributeCount + 1);
+                Arrays.stream(kMeans[k]).forEach(x->{
+                    x = new BigDecimal(x).setScale(2, RoundingMode.HALF_UP).doubleValue();
+                });
                 k++;
             }
         }
-        System.out.println(kMeans);
         return kMeans;
     }
 
     public void runKMeans(double[][] kMeans) {
         boolean converged = false;
         int iterations = 1;
-
         while (!converged && iterations <= maxIterations) {
+            System.out.println("Running K-Means iteration:" + iterations);
             for (int i = 0; i < dataMatrix.length; i++) {
                 double squaredDistance = 0;
                 double eucdDis = 0;
@@ -106,7 +112,7 @@ public class KMeans {
                 int minClusterIndex = 0;
                 for (int k = 0; k < kMeans.length; k++) {
                     for (int j = 1; j <= attributeCount; j++) {
-                        squaredDistance += Math.pow(dataMatrix[i][j] - kMeans[k][j], 2);
+                        squaredDistance += Math.pow(dataMatrix[i][j] - kMeans[k][j-1], 2);
                     }
                     eucdDis = Math.sqrt(squaredDistance);
                     if (eucdDis < minDistance) {
@@ -115,8 +121,39 @@ public class KMeans {
                     }
                     squaredDistance = 0;
                 }
-                System.out.println("Data Point: " + dataMatrix[i][0] + " Min Distance->" + minDistance + " ClusterIndex->" + minClusterIndex);
+                dataMatrix[i][clusterIndex] = minClusterIndex;
             }
+            double[][] newKMeans = new double[kMeans.length][attributeCount];
+            for (int k = 0; k < kMeans.length; k++) {
+                double objectCluster = k + 1;
+                List<double[]> clusterObjects = Arrays.stream(dataMatrix).filter(x -> x[clusterIndex] == objectCluster).collect(Collectors.toList());
+                for (int col = 1; col <= attributeCount; col++) {
+                    double sum = 0;
+                    for (double[] clusterObject : clusterObjects) {
+                        sum += clusterObject[col];
+                    }
+                    double centroid = sum / clusterObjects.size();
+                    newKMeans[k][col-1] = new BigDecimal(centroid).setScale(2, RoundingMode.HALF_UP).doubleValue();
+                }
+            }
+            if (checkConvergence(kMeans, newKMeans)) converged = true;
+            iterations++;
+            kMeans = newKMeans;
+        }
+        System.out.println("K-Means converged after " + (iterations-1) + " iterations");
+        printClusters(kMeans);
+    }
+
+    private boolean checkConvergence(double[][] oldKMeans, double[][] newKMeans) {
+        return Arrays.deepEquals(oldKMeans, newKMeans);
+    }
+
+    private void printClusters(double[][] kMeans){
+        for(int i=0;i<kMeans.length; i++){
+            double clsIndx = i + 1;
+            List<double[]> clusterObjects = Arrays.stream(dataMatrix).filter(x -> x[clusterIndex] == clsIndx).collect(Collectors.toList());
+            Arrays.stream(dataMatrix).filter(x -> x[clusterIndex] == clsIndx).collect(Collectors.toList());
+            System.out.println("Cluster "+new Double(clsIndx).intValue()+" size: "+clusterObjects.size());
         }
     }
 
