ebf9d34f3889cc07fae934c7e52b7bcb8ada5061
nitish <goelnitish@gmail.com>
2016-10-31 15:46:57 -0400

    work in progress

--- src/com/ub/cse601/project2/KMeansMapReduce/KMeansMR.java
+++ src/com/ub/cse601/project2/KMeansMapReduce/KMeansMR.java
@@ -4,4 +4,36 @@ package com.ub.cse601.project2.KMeansMapReduce;
  * Created by nitish on 10/31/16.
  */
 public class KMeansMR {
+
+    private int initialCentroids = 5;
+
+    public void writeInitialCentroidsToFile ( String path ) {
+
+        try {
+
+
+
+        } catch ( Exception e ) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+
+    public static void main ( String[] args ) {
+
+        /*
+        * choose 5 initial centroids and store them in a file
+        * sanitize gene data and store them in a separate file
+        * */
+
+
+
+
+
+
+    }
+
 }
